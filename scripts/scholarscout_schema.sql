7324411dbb4d0e78e7d320b29821fc65ef735f25
harisbuilds <aliharris727@gmail.com>
2026-05-02 23:14:36 +0500

    chore: update db schema

--- scripts/scholarscout_schema.sql
+++ scripts/scholarscout_schema.sql
@@ -3,7 +3,7 @@
 CREATE database scholarscout
 
 
-CREATE TABLE IF NOT EXISTS universities (
+CREATE TABLE universities (
     id              SERIAL PRIMARY KEY,
     program_name    TEXT NOT NULL,
     university_name TEXT NOT NULL,
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS universities (
 );
 
 
+CREATE TABLE finances (
+	university_id int4 NOT NULL,
+	tuition_fee_eu_per_year int4 NULL,
+	tuition_fee_non_eu_per_year int4 NULL,
+	scholarship_available bool NULL,
+	funding_category jsonb NULL,
+	urls jsonb NULL,
+	CONSTRAINT finances_pkey PRIMARY KEY (university_id),
+	CONSTRAINT finances_university_id_fkey FOREIGN KEY (university_id) REFERENCES public.universities(id)
+);
